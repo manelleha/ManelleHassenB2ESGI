9347fed25a1600ce3050c84298f8575132a4bb76
manelleha <94798202+manelleha@users.noreply.github.com>
2023-03-10 00:23:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('hello')
+// exercice 1
+let tableau = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < tableau.length; i++)
+{
+  console.log(tableau[i]);
+}
+// exercice 2 
+let tableau2 = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'];
+
+    console.log(tableau2[0]);
+
+
